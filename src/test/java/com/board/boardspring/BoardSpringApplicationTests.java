f5340a42b0a941828bb855c040078b5381c471fe
kimgwanjung <qwe2846@naver.com>
2023-08-07 23:46:26 +0900

    first commit

---
+++ src/test/java/com/board/boardspring/BoardSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.board.boardspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
