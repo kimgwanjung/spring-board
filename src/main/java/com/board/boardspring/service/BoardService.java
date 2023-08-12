8c7a6bbadd4ed2ca3f8602d39aacfb0e2bc2a2ef
kimgwanjung <qwe2846@naver.com>
2023-08-12 16:53:27 +0900

    spring 기반 게시판

---
+++ src/main/java/com/board/boardspring/service/BoardService.java
@@ -0,0 +1,41 @@
+package com.board.boardspring.service;
+
+import com.board.boardspring.domain.entity.Board;
+import com.board.boardspring.domain.repository.BoardRepository;
+import com.board.boardspring.dto.BoardDto;
+import jakarta.transaction.Transactional;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class BoardService {
+    private final BoardRepository boardRepository;
+
+    public BoardService(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Transactional
+    public Long savePost(BoardDto boardDto) {
+        return boardRepository.save(boardDto.toEntity()).getId();
+    }
+    @Transactional
+    public List<BoardDto> getBoardList() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+
+        for(Board board : boardList) {
+            BoardDto boardDto = BoardDto.builder()
+                    .id(board.getId())
+                    .author(board.getAuthor())
+                    .title(board.getTitle())
+                    .content(board.getContent())
+                    .createdDate(board.getCreatedDate())
+                    .build();
+            boardDtoList.add(boardDto);
+        }
+        return boardDtoList;
+    }
+}
