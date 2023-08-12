3021d8151e753cbe0ab85ba3886b8654f7558009
kimgwanjung <qwe2846@naver.com>
2023-08-12 17:35:25 +0900

    글 수정 추가

--- src/main/java/com/board/boardspring/controller/BoardController.java
+++ src/main/java/com/board/boardspring/controller/BoardController.java
@@ -1,10 +1,11 @@
 package com.board.boardspring.controller;
 
-import org.springframework.ui.Model;
 import com.board.boardspring.dto.BoardDto;
 import com.board.boardspring.service.BoardService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
@@ -33,5 +34,12 @@ public class BoardController {
     public String write(BoardDto boardDto) {
         boardService.savePost(boardDto);
         return "redirect:/";
+    }
+
+    @GetMapping("/post/{id}")
+    public String detail(@PathVariable("id") Long id, Model model) {
+        BoardDto boardDto = boardService.getPost(id);
+        model.addAttribute("post", boardDto);
+        return "board/detail.html";
     }
 }
